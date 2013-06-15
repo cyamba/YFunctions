069c55ada374a32414e52abdb9ef7dc4e1b11324
cyamba <christopher.yamba@gmail.com>
2013-06-15 20:19:35 +0200

    unit test started

--- src/test/java/com/seewhy/yfunctions/structure/YListTest.java
+++ src/test/java/com/seewhy/yfunctions/structure/YListTest.java
@@ -1,9 +1,65 @@
 package com.seewhy.yfunctions.structure;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * @author cbyamba
  *         Date: 2013-06-15
  *         Time: 19:48
  */
 public class YListTest {
+
+    YVoid<Tuple<Integer, String>> printer = new YVoid<Tuple<Integer, String>>() {
+        public Nil f(Tuple<Integer, String> tuple) {
+            System.out.print(tuple.toString() + " ");
+            return new Nil();
+        }
+    };
+
+    @Test
+    public void test() {
+        YList<Integer> numbers = YList.list(1, 2, 3, 4, 5);
+        YList<String> alphabeth = YList.list("a", "b", "c", "d", "e");
+        YList<Tuple<Integer, String>> tuples = YLists.zip(numbers, alphabeth);
+        print(tuples.length() + " " + Arrays.deepToString(tuples.toArray()));
+        nl();
+        tuples.foreach(printer);
+        nl();
+        print(tuples.split().toString());
+    }
+
+    @Test
+    public void testMultiFilter() {
+        YList<Integer> numbers = YList.list(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).merge(YList.list(11, 22, 33, 44, 55, 66, 77));
+        numbers
+                .filter(
+                        new Predicate<Integer>() {
+                            public boolean f(Integer integer) {
+                                return integer % 2 != 0;
+                            }
+                        },
+                        new Predicate<Integer>() {
+                            public boolean f(Integer integer) {
+                                return integer % 3 != 0;
+                            }
+                        }
+                )
+                .map(new Binad<Integer, String>() {
+                    public String f(Integer integer) {
+                        return Integer.toBinaryString(integer);
+                    }
+                })
+                .print();
+    }
+
+    void nl() {
+        System.out.println();
+    }
+
+    void print(String s) {
+        System.out.println(s);
+    }
 }
+
