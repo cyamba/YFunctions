84ec28febf4fb08c3e67c6b7d74ba79169cdd25b
cyamba <christopher.yamba@gmail.com>
2013-06-15 19:51:54 +0200

    dtm

---
+++ src/main/java/com/seewhy/yfunctions/structure/YList.java
@@ -0,0 +1,10 @@
+package com.seewhy.yfunctions.structure;
+
+/**
+ * @author cbyamba
+ *         Date: 2013-06-15
+ *         Time: 19:48
+ */
+public class YList {
+
+}
---
+++ src/test/java/com/seewhy/yfunctions/structure/YListTest.java
@@ -0,0 +1,11 @@
+package com.seewhy.yfunctions.structure;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cbyamba
+ * Date: 2013-06-15
+ * Time: 19:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class YListTest {
+}
